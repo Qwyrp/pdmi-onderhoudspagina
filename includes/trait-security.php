997c513c34a30cb248f0b3a185bd4c2b35408d76
Qwyrp <pedemo@gmx.com>
2026-01-06 14:50:47 +0100

    Zowel IPV4 als IPV6 mogelijk om toe te voegen

--- includes/trait-security.php
+++ includes/trait-security.php
@@ -28,22 +28,29 @@ trait Security {
 	/**
 	 * Sanitizes comma-separated IP list.
 	 *
-	 * @param string $ip_list Comma-separated IP string.
+	 * @param string $ip_list Comma-separated IP string. Supports both IPv4 and IPv6.
 	 *
 	 * @return array
 	 */
 	protected function sanitize_ip_list( $ip_list ) {
 		$validated = array();
 		$ip_list   = (string) $ip_list;
-		$ips       = array_filter(
-			array_map(
-				'trim',
-				explode( ',', $ip_list )
-			)
-		);
+
+		// Allow IP's to be separated by comma's, spaces of nieuwe regels.
+		$ips = preg_split( '/[\s,]+/', $ip_list );
+		$ips = array_filter( array_map( 'trim', (array) $ips ) );
 
 		foreach ( $ips as $ip ) {
-			$filtered_ip = filter_var( $ip, FILTER_VALIDATE_IP );
+			// Accepteer expliciet zowel IPv4 als IPv6 adressen.
+			$filtered_ip = filter_var(
+				$ip,
+				FILTER_VALIDATE_IP,
+				array(
+					'options' => array(),
+					'flags'   => FILTER_FLAG_IPV4 | FILTER_FLAG_IPV6,
+				)
+			);
+
 			if ( false !== $filtered_ip ) {
 				$validated[] = $filtered_ip;
 			}
